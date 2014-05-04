2ef286231855faf684671617ad3c98edcf39d7c7
Aditya Bhargava <bluemangroupie@gmail.com>
2014-05-03 22:52:23 -0700

    move types into Types.hs, and add a LevelComplete type to GameState

--- src/Chips/Types.hs
+++ src/Chips/Types.hs
@@ -1,2 +1,78 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Chips.Types where
-import Chips.Common  
+import Chips.Common
+import Control.Lens
+import ActionKid
+import Graphics.Gloss hiding (display)
+
+data Tile = Empty Attributes
+          | Wall  Attributes
+          | Chip  Attributes
+          | KeyYellow Attributes
+          | KeyRed Attributes
+          | KeyGreen Attributes
+          | KeyBlue Attributes
+          | LockYellow Attributes
+          | LockRed Attributes
+          | LockGreen Attributes
+          | LockBlue Attributes
+          | Gate Attributes
+          | GateFinal Attributes
+          | Help Attributes
+          deriving (Show, Eq)
+
+deriveMC ''Tile
+
+instance Renderable Tile where
+    render (Empty _)      = image "images/empty.png"
+    render (Wall _)       = image "images/wall.png"
+    render (Chip _)       = image "images/chip.png"
+    render (KeyYellow _)  = image "images/key_yellow.png"
+    render (KeyRed _)     = image "images/key_red.png"
+    render (KeyGreen _)   = image "images/key_green.png"
+    render (KeyBlue _)    = image "images/key_blue.png"
+    render (LockYellow _) = image "images/lock_yellow.png"
+    render (LockRed _)    = image "images/lock_red.png"
+    render (LockGreen _)  = image "images/lock_green.png"
+    render (LockBlue _)   = image "images/lock_blue.png"
+    render (Gate _)       = image "images/gate.png"
+    render (GateFinal _)  = image "images/gate_final1.png"
+    render (Help _)       = image "images/help.png"
+
+data Direction = DirUp | DirDown | DirLeft | DirRight
+
+data Player = Player {
+                _direction :: Direction,
+                _ar :: Attributes
+}
+
+makeLenses ''Player
+deriveMC ''Player
+
+instance Renderable Player where
+    render p = case p ^. direction of
+                 DirUp    -> image "images/player_up.png"
+                 DirDown  -> image "images/player_down.png"
+                 DirLeft  -> image "images/player_left.png"
+                 DirRight -> image "images/player_right.png"
+
+data GameState = GameState {
+                    _tiles :: [Tile],
+                    _player :: Player,
+                    _level :: Int,
+                    _redKeyCount :: Int,
+                    _blueKeyCount :: Int,
+                    _yellowKeyCount :: Int,
+                    _hasGreenKey :: Bool,
+                    _ga :: Attributes
+               } | LevelComplete {
+                    _levelNum :: Int,
+                    _lca :: Attributes
+               }
+
+makeLenses ''GameState
+deriveMC ''GameState
+
+instance Renderable GameState where
+    render (LevelComplete _ _) = translate 0 200 $ scale 0.1 0.1 $ text "Level Complete! Press space to go onward!"
+    render gs = displayAll (_tiles gs) <> display (_player gs)
--- src/Main.hs
+++ src/Main.hs
@@ -5,78 +5,9 @@ import Data.Monoid ((<>), mconcat)
 import Chips
 import Control.Lens
 import Graphics.Gloss hiding (display)
-import qualified Debug.Trace as D
-import Data.Maybe
 import Chips.Utils
 
 tileSize = 32
-
-data Tile = Empty Attributes
-          | Wall  Attributes
-          | Chip  Attributes
-          | KeyYellow Attributes
-          | KeyRed Attributes
-          | KeyGreen Attributes
-          | KeyBlue Attributes
-          | LockYellow Attributes
-          | LockRed Attributes
-          | LockGreen Attributes
-          | LockBlue Attributes
-          | Gate Attributes
-          | GateFinal Attributes
-          | Help Attributes
-          deriving (Show, Eq)
-
-deriveMC ''Tile
-
-instance Renderable Tile where
-    render (Empty _)      = image "images/empty.png"
-    render (Wall _)       = image "images/wall.png"
-    render (Chip _)       = image "images/chip.png"
-    render (KeyYellow _)  = image "images/key_yellow.png"
-    render (KeyRed _)     = image "images/key_red.png"
-    render (KeyGreen _)   = image "images/key_green.png"
-    render (KeyBlue _)    = image "images/key_blue.png"
-    render (LockYellow _) = image "images/lock_yellow.png"
-    render (LockRed _)    = image "images/lock_red.png"
-    render (LockGreen _)  = image "images/lock_green.png"
-    render (LockBlue _)   = image "images/lock_blue.png"
-    render (Gate _)       = image "images/gate.png"
-    render (GateFinal _)  = image "images/gate_final1.png"
-    render (Help _)       = image "images/help.png"
-
-data Direction = DirUp | DirDown | DirLeft | DirRight
-
-data Player = Player {
-                _direction :: Direction,
-                _ar :: Attributes
-}
-
-makeLenses ''Player
-deriveMC ''Player
-
-instance Renderable Player where
-    render p = case p ^. direction of
-                 DirUp    -> image "images/player_up.png"
-                 DirDown  -> image "images/player_down.png"
-                 DirLeft  -> image "images/player_left.png"
-                 DirRight -> image "images/player_right.png"
-
-data GameState = GameState {
-                    _tiles :: [Tile],
-                    _player :: Player,
-                    _redKeyCount :: Int,
-                    _blueKeyCount :: Int,
-                    _yellowKeyCount :: Int,
-                    _hasGreenKey :: Bool,
-                    _ga :: Attributes
-}
-
-makeLenses ''GameState
-deriveMC ''GameState
-
-instance Renderable GameState where
-    render gs = displayAll (_tiles gs) <> display (_player gs)
 
 tileMap = 
     [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -101,8 +32,8 @@ boardH = length tileMap
 
 playerCoords :: GameState -> (Int, Int)
 playerCoords gs = ((floor (p ^. x)) // tileSize, (((boardH * tileSize) - (floor (p ^. y))) // tileSize)-1)
-    where p = gs ^. player
-          ts = gs ^. tiles
+    where p = _player gs
+          ts = _tiles gs
 
 currentIdx :: GameState -> Int
 currentIdx gs = y_ * boardW + x_
@@ -130,7 +61,7 @@ renderedTiles = renderTileMap tileMap f (tileSize, tileSize)
           f 13 = GateFinal def
           f 14 = Help def
 
-gameState = GameState renderedTiles (x .~ (8*tileSize) $ y .~ (8*tileSize) $ player_) 0 0 0 False def
+gameState = GameState renderedTiles (x .~ (8*tileSize) $ y .~ (8*tileSize) $ player_) 1 0 0 0 False def
         where player_ = (Player DirDown def)
 
 main = run "chips challenge" (9 * tileSize, 9 * tileSize) (x -~ (4*tileSize) $ y -~ (4*tileSize) $ gameState) on stepGame
@@ -147,7 +78,7 @@ maybeMove func gs newGs =
       LockBlue _   -> if _blueKeyCount gs > 0 then newGs else gs
       LockGreen _  -> if _hasGreenKey gs then newGs else gs
       LockYellow _ -> if _yellowKeyCount gs > 0 then newGs else gs
-      Gate _       -> if chipsLeft gs == 0 then newGs else gs
+      Gate _       -> newGs --if chipsLeft gs == 0 then newGs else gs
       _ -> newGs
 
 on (EventKey (SpecialKey KeyLeft) Down _ _) gs =
@@ -183,6 +114,7 @@ on (EventKey (SpecialKey KeySpace) Down _ _) gs =
 on _ gs =
     return $ player.direction .~ DirDown $ gs
 
+stepGame _ gs@(LevelComplete _ _) = return gs
 stepGame _ gs = do
     let playerIx = currentIdx gs
     let attrs_ = ((gs ^. tiles) !! playerIx) ^. attrs
@@ -198,4 +130,7 @@ stepGame _ gs = do
       LockBlue _ -> return $ blueKeyCount -~ 1 $ resetTile playerIx
       LockGreen _ -> return $ resetTile playerIx
       LockRed _ -> return $ redKeyCount -~ 1 $ resetTile playerIx
+      GateFinal _ -> do
+        let lvl = _level gs
+        return $ LevelComplete lvl def
       _ -> return gs
