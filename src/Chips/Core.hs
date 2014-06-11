43c3608fe74ece7b1b237e0457f8380d41a31046
Aditya Bhargava <bluemangroupie@gmail.com>
2014-06-10 22:38:56 -0700

    bug fix

--- src/Chips/Core.hs
+++ src/Chips/Core.hs
@@ -507,6 +507,7 @@ checkCurTile (Ice _) = do
     DirDown -> bounceCheck TileBelow DirUp
     DirLeft -> bounceCheck TileLeft DirRight
     DirRight -> bounceCheck TileRight DirLeft
+    Standing -> return ()
 
 checkCurTile (IceBottomLeft _) = do
   gs <- get
